8e171043af5035d8c1abd6cc471a0cbcfb449a7e
creativebcx <ericdonley1@gmail.com>
2017-09-23 20:26:05 -0600

    test-data

---
+++ testdata.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const demousers = {
+  'user0': {
+    'userId': 1234,
+    'userName': 'TestUser1234',
+    'userPass': 'TestUserPass1234',
+    'userPhone': '555-555-5555',
+    'userEmail': 'TestEmail1234@fakeEmail.com',
+    'userLinks': [
+      'T1234',
+      'T4321',
+      'T1111'
+    ]
+  }
+}
+
+const demoTools = {
+  'T1234': {
+    'toolName': 'Skill Saw',
+    'toolQuality': 'Fair',
+    'toolPrice': '25',
+    'tollPriceUnite': 'day',
+    'toolDescription': 'Test Lorum Ipsum Verde Talle',
+    'toolImgUrl': 'img/testimgskillsaw.jpg',
+    'toolLocation': 'Boise',
+    'toolProjectType': 'Carpentry',
+    'toolType': 'Electric Saws',
+    'toolOwner': '1234',
+    'toolAvailable': true
+  },
+  'T4321': {
+    'toolName': 'Finishing Sander',
+    'toolQuality': 'Excellent',
+    'toolPrice': '15',
+    'tollPriceUnite': 'day',
+    'toolDescription': 'Test Lorum Ipsum Verde Talle',
+    'toolImgUrl': 'img/testimgfinishingsander.jpg',
+    'toolLocation': 'Boise',
+    'toolProjectType': 'Carpentry',
+    'toolType': 'Electric Sanders',
+    'toolOwner': '1234',
+    'toolAvailable': true
+  },
+  'T1111': {
+    'toolName': 'Car Jack',
+    'toolQuality': 'Good',
+    'toolPrice': '10',
+    'tollPriceUnite': 'day',
+    'toolDescription': 'Test Lorum Ipsum Verde Talle',
+    'toolImgUrl': 'img/testimgautojack.jpg',
+    'toolLocation': 'Boise',
+    'toolProjectType': 'Auto',
+    'toolType': 'Jackstands/Lifts',
+    'toolOwner': '1234',
+    'toolAvailable': true
+  }
+}
+
